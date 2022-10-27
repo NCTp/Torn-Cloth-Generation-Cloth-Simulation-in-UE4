2c79dca0b889f27f1d7fdd2ad5afeaa9a95ac9ef
NCTP <cjfgh530@naver.com>
2022-10-27 21:46:10 +0900

    link with CUDA 11.6
    link with CUDA 11.6

---
+++ MyClothSimulation/Source/MyClothSimulation/CUDATESTACTOR.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CUDATESTACTOR.h"
+
+// Sets default values
+ACUDATESTACTOR::ACUDATESTACTOR()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACUDATESTACTOR::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACUDATESTACTOR::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
